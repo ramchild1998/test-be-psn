6e90f3711a4973fb762f02ca84c5e9d5feef36b1
ramchild1998 <ramadiansyah1998@gmail.com>
2024-07-11 20:11:58 +0700

    route api added

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\API\AddressController;
+use App\Http\Controllers\API\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/customer', [CustomerController::class, 'index']);
+Route::get('/customer/{id}', [CustomerController::class, 'show']);
+Route::post('/customer', [CustomerController::class, 'store']);
+Route::patch('/customer/{id}', [CustomerController::class, 'update']);
+Route::delete('/customer/{id}', [CustomerController::class, 'destroy']);
+
+Route::post('/address', [AddressController::class, 'store']);
+Route::patch('/address/{id}', [AddressController::class, 'update']);
+Route::delete('/address/{id}', [AddressController::class, 'destroy']);
+
