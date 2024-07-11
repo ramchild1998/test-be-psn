0daf0973af3bd3e1f747f9215f073b5acec630ea
ramchild1998 <ramadiansyah1998@gmail.com>
2024-07-11 19:52:46 +0700

    update migrations customer stores and address

--- database/migrations/2024_07_11_095037_customer_stores.php
+++ database/migrations/2024_07_11_095037_customer_stores.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('customer_stores');
+        Schema::dropIfExists('customers');
     }
 };
